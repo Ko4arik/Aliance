f7f59b194d9f117d3668ccc6c5658cec4e367642
Ko4arik <ekocharovsky@gmail.com>
2023-10-13 13:28:45 +0300

    fix: navabr scroll

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  if (this.scrollY > 1) {
+    navbar.classList.add("navbar-light");
+  } else 
+  navbar.classList.remove("navbar-light");
+});
